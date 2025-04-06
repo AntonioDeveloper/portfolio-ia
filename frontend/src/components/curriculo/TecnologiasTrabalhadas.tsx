75874cfda439050ce37052305aa41327d1f541b0
Antonio <antonio.junior@original.io>
2025-04-06 19:45:02 -0300

    feat(Mini CV): Finished

--- frontend/src/components/curriculo/TecnologiasTrabalhadas.tsx
+++ frontend/src/components/curriculo/TecnologiasTrabalhadas.tsx
@@ -6,7 +6,7 @@ export interface TecnologiasTrabalhadasProps {
 
 export default function TecnologiasTrabalhadas(props: TecnologiasTrabalhadasProps) {
 return props.lista ? (
-  <div className="flex justify-center items-center p-6 w-full lg:w-72 bg-black border border-zinc-800">
+  <div className="flex justify-center items-center p-6 w-full lg:w-72 bg-black border border-zinc-800 rounded-2xl">
     <div className="flex justify-center flex-wrap text-white gap-x-3">
           {props.lista.map((tecnologia) => (
         <div key={tecnologia.id} className="flex justify-center flex-wrap text-white gap-x-3">
